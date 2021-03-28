90f21ab0b5f854de74c1a99b24ae2e6f66118a66
Sam Weller <samweller@Sams-MacBook-Pro.local>
2021-03-28 13:08:38 -0400

    Added tag endpoints

--- app/core/tests/test_models.py
+++ app/core/tests/test_models.py
@@ -1,6 +1,15 @@
 from django.test import TestCase
 from django.contrib.auth import get_user_model
 
+
+from core import models
+
+
+def sample_user(email='sam@weller.com', password='testpass'):
+    """Create a sample_user"""
+    return get_user_model().objects.create_user(email,password)
+
+
 class ModelTests(TestCase):
     def test_create_user_with_email_sucessful(self):
         """Test creating new user with an email is successful"""
@@ -35,3 +44,12 @@ class ModelTests(TestCase):
 
         self.assertTrue(user.is_superuser)
         self.assertTrue(user.is_staff)
+
+    def test_tag_str(self):
+        """Test the tag string"""
+        tag = models.Tag.objects.create(
+            user=sample_user(),
+            name='Vegan'
+        )
+
+        self.assertEqual(str(tag),tag.name)
